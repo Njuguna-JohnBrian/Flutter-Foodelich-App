4e36534906f7e386dd9b551c4c8dbe8a40bc5d1b
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-06-17 15:26:55 +0300

    Create Grocery Item Screen

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -27,12 +27,82 @@ class GroceryItemScreen extends StatefulWidget {
 }
 
 class _GroceryItemScreenState extends State<GroceryItemScreen> {
-  // todo: Add grocery item screen state properties
+  final _nameController = TextEditingController();
+  String _name = '';
+
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+
+  int _currentSliderValue = 0;
+  @override
+  void initState() {
+    final originalItem = widget.originalItem;
+
+    if (originalItem != null) {
+      _nameController.text = originalItem.name;
+      _name = originalItem.name;
+      _currentSliderValue = originalItem.quantity;
+      _importance = originalItem.importance;
+      _currentColor = originalItem.color;
+
+      final date = originalItem.date;
+
+      _timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+
+      _dueDate = date;
+    }
+
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // todo 12: add groceryitemscreen scaffold
-    return Container(
-      color: Colors.orange,
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.check),
+            onPressed: () {
+              // todo 24: add callback handler
+            },
+          ),
+        ],
+        elevation: 0.0,
+        title: Text(
+          'Grocery Item',
+          style: GoogleFonts.lato(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(
+          16.0,
+        ),
+        child: ListView(children: [
+          // todo 13: add name textfield 
+          // todo 14: add importance selection 
+          // todo 15: add date picker 
+          // todo 16: add time picker 
+          // todo 17: add color picker 
+          // todo 18: add slider
+          // todo 19: add grocery tile
+        ],),
+      ),
     );
   }
   // todo:add buildNameField()
